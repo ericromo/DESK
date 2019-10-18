3ce6fef54a4e75d374f6b2690f6f01e7052c25d0
Kian <45096562+Kimara9@users.noreply.github.com>
2019-10-18 15:37:24 -0700

    Added functionality for buttons in page2 for Eric

--- Navigation/Navigation/View/Page2.xaml.cs
+++ Navigation/Navigation/View/Page2.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Fingerprint;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Navigation.View
@@ -9,6 +15,25 @@ namespace Navigation.View
         public Page2()
         {
             InitializeComponent();
+            btnBlue.Clicked += BtnBlue_Clicked;
+            btnGreen.Clicked += BtnGreen_Clicked1;
+            btnRed.Clicked += BtnRed_Clicked;
+        }
+
+        private async void BtnRed_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Red", "You've clicked the red button", "OK");
+        }
+
+        private async void BtnBlue_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Blue", "You've clicked the blue button", "OK");
+        }
+
+        private async void BtnGreen_Clicked1(object sender, EventArgs e)
+        {
+            await DisplayAlert("ERIC ROMO", "PROFESSIONAL AUTOMATON", "CASEY PLS");
         }
     }
+
 }
